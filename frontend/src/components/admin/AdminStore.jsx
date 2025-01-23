71cc00414623d8fcc4a433fab9865e288508927e
K G K Fernando <kalharageethaka12@gmail.com>
2025-01-23 21:42:47 +0530

    admin supplement store
    Admin supplement store part created.

--- frontend/src/components/admin/AdminStore.jsx
+++ frontend/src/components/admin/AdminStore.jsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import AdminProductsPage from './AdminProductManagement';
+import AdminOrdersPage from './AdminOrderManagement';
+
 function AdminStore() {
   return (
-    <div>AdminStore</div>
-  )
+      <Routes>
+        <Route path="/" element={<AdminProductsPage />} />
+        <Route path="products" element={<AdminProductsPage />} />
+        <Route path="orders" element={<AdminOrdersPage />} />
+      </Routes>
+  );
 }
 
-export default AdminStore
+export default AdminStore;
