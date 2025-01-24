892fc314f22e975ce222188d2ec539893ea5f6f8
K G K Fernando <kalharageethaka12@gmail.com>
2025-01-24 21:34:51 +0530

    admin user management
    Admin user management both frontend and backend

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,28 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {
-  registerAdmin,
-  loginAdmin,
-  getAdminProfile,
-  updateAdminProfile,
-} = require('../controllers/adminController');
-const {
-  createDietPlan,
-  getAllDietPlans,
-  getDietPlan,
-  updateDietPlan,
-  deleteDietPlan
-} = require('../controllers/adminDietPlanController');
-const {
-  getAllPosts,
-  getPostAnalytics,
-  deletePost,
-  getPostDetails
-} = require('../controllers/adminPostController');
+const { registerAdmin, loginAdmin, getAdminProfile, updateAdminProfile } = require('../controllers/adminController');
+const {createDietPlan, getAllDietPlans, getDietPlan, updateDietPlan, deleteDietPlan } = require('../controllers/adminDietPlanController');
+const { getAllPosts, getPostAnalytics, deletePost, getPostDetails } = require('../controllers/adminPostController');
 const productController = require('../controllers/adminProductController');
 const orderController = require('../controllers/adminOrderController');
 const upload = require('../config/uploadProduct');
 const adminAuth = require('../middlewares/adminAuth');
+const adminUserController = require('../controllers/adminUserController');
 
 
 // Admin Authentication Routes
@@ -55,5 +40,12 @@ router.get('/orders', adminAuth, orderController.getAllOrders);
 router.put('/orders/:id/status', adminAuth, orderController.updateOrderStatus);
 router.put('/orders/:id/cancel', adminAuth, orderController.cancelOrder);
 router.delete('/orders/:id/delete', adminAuth, orderController.deleteCancelledOrder);
+
+// User routes
+router.get('/users', adminAuth, adminUserController.getAllUsers);
+router.get('/users/:id', adminAuth, adminUserController.getUserById);
+router.put('/users/:id', adminAuth, adminUserController.updateUserById);
+router.delete('/users/:id', adminAuth, adminUserController.deleteUserById);
+router.get('/users/search', adminAuth, adminUserController.searchUsers);
 
 module.exports = router;
